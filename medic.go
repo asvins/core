22b6d318f813d271a3408f47e4646c9bb3a98637
Vinicius Correa <viniciuscorrea91@gmail.com>
2015-12-04 15:37:20 -0200

    Added medics basic model

---
+++ medic.go
@@ -0,0 +1,29 @@
+package main
+
+import "github.com/jinzhu/gorm"
+
+type Medic struct {
+	gorm.Model
+	Name      string `json:"name" gorm:"column:name"`
+	CRM       string `json:"crm" gorm:"column:crm"`
+	CPF       string `json:"cpf" gorm:"column:cpf"`
+	Specialty string `json:"specialty" gorm:"column:specialty"`
+	Email     string `json:"email" gorm:"column:email"`
+	Avatar    []byte `json:"avatar" gorm:"column:avatar"`
+}
+
+func (m *Medic) Create() error {
+	return db.Create(m).Error
+}
+
+func (m *Medic) Update() error {
+	return db.Save(m).Error
+}
+
+func FindMedicByID(id string, m Medic) error {
+	return db.Where("id = ?").First(&m).Error
+}
+
+func ListMedics(ms []Medic) error {
+	return db.Where("").Find(&ms).Error
+}

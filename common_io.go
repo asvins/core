3b6ae645be3b542e9ab19321120d8a4ffbb32d83
Rafael Leite <r.cmgleite@gmail.com>
2015-12-05 22:51:20 -0200

    handling user creation kafka topic

--- common_io.go
+++ common_io.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 
+	"github.com/asvins/auth/models"
 	"github.com/asvins/common_io"
 	"github.com/asvins/utils/config"
 )
@@ -26,15 +29,67 @@ func setupCommonIo() {
 	/*
 	*	Consumer
 	 */
-	//consumer = common_io.NewConsumer(cfg)
-	//consumer.HandleTopic("", nil)
+	consumer = common_io.NewConsumer(cfg)
 
-	//if err = consumer.StartListening(); err != nil {
-	//	log.Fatal(err)
-	//}
+	/*
+	*	Topics
+	 */
+
+	// users (patient, medic, pharmaceutic)
+	consumer.HandleTopic("user_created", handleUserCreated)
+
+	// medication TODO
+	consumer.HandleTopic("medication_created", nil)
+	consumer.HandleTopic("medication_updated", nil)
+	consumer.HandleTopic("medication_deleted", nil)
+
+	if err = consumer.StartListening(); err != nil {
+		log.Fatal(err)
+	}
 
 }
 
 /*
 *	Here can be added the handlers for kafka topics
  */
+
+func handleUserCreated(msg []byte) {
+	var usr models.User
+	err := json.Unmarshal(msg, &usr)
+
+	if err != nil {
+		fmt.Println("[ERROR] ", err.Error())
+	}
+
+	switch usr.Scope {
+	case "patient":
+		p := Patient{}
+		p.Name = usr.FirstName + " " + usr.LastName
+		p.Email = usr.Email
+
+		err = p.Save()
+
+		return
+
+	case "medic":
+		m := Medic{}
+		m.Name = usr.FirstName + " " + usr.LastName
+		m.Email = usr.Email
+
+		err = m.Create()
+
+		return
+
+	case "pharmaceutic":
+		p := Pharmaceutic{}
+		p.Name = usr.FirstName + " " + usr.LastName
+		p.Email = usr.Email
+
+		err = p.Save()
+		return
+	}
+	if err != nil {
+		fmt.Println("[ERROR] ", err.Error())
+	}
+
+}
